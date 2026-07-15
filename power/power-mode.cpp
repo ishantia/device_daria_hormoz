2fd403f31cd9bb6c7b41dca758ec62da4314211a
ishantia <shantiaeslami@gmail.com>
2026-07-15 12:49:53 +0330

    Remove remaining Xiaomi property references and stub out power-mode touch driver calls

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -4,17 +4,6 @@
  */
 
 #include <aidl/android/hardware/power/BnPower.h>
-#include <android-base/file.h>
-#include <android-base/logging.h>
-#include <sys/ioctl.h>
-
-#define SET_CUR_VALUE 0
-#define TOUCH_DOUBLETAP_MODE 14
-#define TOUCH_FOD_ENABLE 10
-#define TOUCH_MAGIC 't'
-#define TOUCH_IOC_SETMODE _IO(TOUCH_MAGIC, SET_CUR_VALUE)
-#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
-#define TOUCH_ID 0
 
 namespace aidl {
 namespace google {
@@ -27,31 +16,14 @@ using ::aidl::android::hardware::power::Mode;
 
 bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
     switch (type) {
-        case Mode::DOUBLE_TAP_TO_WAKE:
-        case Mode::DISPLAY_INACTIVE:
-            *_aidl_return = true;
-            return true;
         default:
-            return false;
+            *_aidl_return = false;
+            return true;
     }
 }
 
 bool setDeviceSpecificMode(Mode type, bool enabled) {
     switch (type) {
-        case Mode::DOUBLE_TAP_TO_WAKE: {
-            int fd = open(TOUCH_DEV_PATH, O_RDWR);
-            int arg[3] = {TOUCH_ID, TOUCH_DOUBLETAP_MODE, enabled ? 1 : 0};
-            ioctl(fd, TOUCH_IOC_SETMODE, &arg);
-            close(fd);
-            return true;
-        }
-        case Mode::DISPLAY_INACTIVE: {
-            int fd = open(TOUCH_DEV_PATH, O_RDWR);
-            int arg[3] = {TOUCH_ID, TOUCH_FOD_ENABLE, enabled ? 1 : 0};
-            ioctl(fd, TOUCH_IOC_SETMODE, &arg);
-            close(fd);
-            return true;
-        }
         default:
             return false;
     }
